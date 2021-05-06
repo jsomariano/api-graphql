c153485df1ce485290dc38b434b73a178de9259d
Junior <junior_santos2100@outlook.com>
2021-05-05 22:58:38 -0300

    Melhorado codigo e concluido modulo de schemas & querys

--- index.js
+++ index.js
@@ -1,104 +1,11 @@
-import { ApolloServer, gql } from 'apollo-server'
+import { ApolloServer } from 'apollo-server'
+import { importSchema } from "graphql-import"
+import resolvers from './resolvers'
 
-const usuarios = [
-    {
-        id: 1,
-        nome: "Joao Silva",
-        email: "jsilva@zemail.com",
-        idade: 22
-    }, {
-        id: 2,
-        nome: "Maria da silva",
-        email: "Msilva@zemail.com",
-        idade: 23
-    }, {
-        id: 3,
-        nome: "Jose Silva",
-        email: "josilva@zemail.com",
-        idade: 44
-    }
-]
-
-const typeDefs = gql`
-    scalar Date
-
-    type Usuario {
-        id: ID
-        nome: String!
-        email: String!
-        idade: Int
-        salario: Float
-        vip: Boolean        
-    }
-
-    type Produto {        
-        nome: String!
-        preco: Float!
-        desconto: Float
-        preco_com_desconto: Float                
-    }
-
-    type Query {
-        ola: String
-        hora_certa: Date
-        usuario_logado: Usuario
-        produto_em_destaque: Produto
-        numeros_mega_sena: [Int!]!
-        usuarios: [Usuario]
-    }`
-
-const resolvers = {
-    Usuario: {
-        salario(usuario) {
-            return usuario.salario_real
-        },
-    },
-    Produto: {
-        preco_com_desconto(produto) {
-            return produto.preco * (1 - produto.desconto)
-        },
-    },
-    Query: {
-        ola() {
-            return "Hello World"
-        },
-        hora_certa() {
-            return new Date()
-        },
-        usuario_logado() {
-            return {
-                id: 1,
-                nome: "Junior Santos",
-                email: "junior_santos@outlook.com",
-                idade: 23,
-                salario_real: (Math.random() * 1000).toFixed(2),
-                vip: true
-            }
-        },
-        produto_em_destaque() {
-            return {
-                nome: "Coca Cola",
-                preco: 10,
-                desconto: 0.2
-            }
-        },
-        numeros_mega_sena() {
-            const ordernaCrecentemente = (a, b) => (a - b)
-            const removeItemsDuplicados = (item, index, array) => { return array.indexOf(item) == index }
-            return Array(6)
-                .fill(0)
-                .map(() => parseInt(Math.random() * 60 + 1))
-                .filter(removeItemsDuplicados)
-                .sort(ordernaCrecentemente)
-        },
-        usuarios() {
-            return usuarios
-        }
-    }
-}
+const schemaPath = './schema/index.graphql'
 
 const server = new ApolloServer({
-    typeDefs,
+    typeDefs: importSchema(schemaPath),
     resolvers
 })
 
---
+++ resolvers/Produto.js
@@ -0,0 +1,8 @@
+module.exports = {
+    precoComDesconto(produto) {
+        if (produto.desconto) {
+            return produto.preco * (1 - produto.desconto)
+        }
+        return produto.preco
+    },
+}
